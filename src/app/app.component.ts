79d544bacfbc09e4346c4188e690a7d48a422067
GiovanniCortes <giocortes19@gmail.com>
2023-08-15 15:40:04 -0400

    testing inline styling on app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  // templateUrl: './app.component.html',
-  template: `<h1>Hello World from inline template!</h1>
-  <p>Angular is awesome</p>
-  `,
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html',
+  // template: `<h1>Hello World from inline template!</h1>
+  // <p>Angular is awesome</p>
+  // <p>Thank you <strong>Gio</strong></p>
+  // `,
+  // styleUrls: ['./app.component.scss'],
+  styles: [
+    'h1{color: green}'
+  ]
 })
 export class AppComponent {
   title = 'hotelInventoryApp';
